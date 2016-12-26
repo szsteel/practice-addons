ba8b29a30e3c87382b613bb23886a3bb7dbdbd66
Steel Chen <steelchen@cognichain.com>
2016-12-26 16:58:30 +0800

    some updates

---
+++ todo_ui/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'User interface improvement to the To-do APP.',
+    'decription': 'User friendly features.',
+    'author': 'Steel Chen',
+    'depends': ['todo_user'],
+    'data': [
+#        'security/ir.model.access.csv',
+        'views/todo_view.xml',
+        'views/todo_menu.xml',
+    ]
+}
